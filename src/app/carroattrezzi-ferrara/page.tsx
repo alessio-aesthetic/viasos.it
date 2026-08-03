e48e11f0ebe989b07fdd5c11ea0f3c5591bf82c5
alessio-aesthetic <alessi.ola.rocchia@gmail.com>
2026-08-04 01:47:15 +0200

    Add Ferrara roadside assistance landing

---
+++ src/app/carroattrezzi-ferrara/page.tsx
@@ -0,0 +1,59 @@
+import type { Metadata } from 'next'
+
+import { BresciaLandingClient } from '../carroattrezzi-brescia/BresciaLandingClient'
+
+const phone = '0532 080 206'
+const tel = '+390532080206'
+const title = 'Carroattrezzi Ferrara 24H | Soccorso Stradale Rapido'
+const description =
+  'Auto ferma a Ferrara? Chiama subito o invia la posizione: ViaSOS individua rapidamente il carroattrezzi disponibile più vicino.'
+
+export const metadata: Metadata = {
+  title,
+  description,
+  alternates: { canonical: '/carroattrezzi-ferrara' },
+  robots: { index: true, follow: true },
+  openGraph: {
+    title,
+    description,
+    url: 'https://viasos.it/carroattrezzi-ferrara',
+    siteName: 'ViaSOS',
+    locale: 'it_IT',
+    type: 'website',
+  },
+}
+
+function JsonLd() {
+  const localBusiness = {
+    '@context': 'https://schema.org',
+    '@type': ['LocalBusiness', 'AutomotiveBusiness'],
+    name: 'ViaSOS Carroattrezzi Ferrara',
+    url: 'https://viasos.it/carroattrezzi-ferrara',
+    telephone: tel,
+    areaServed: ['Ferrara', 'Ferrara e provincia'],
+    openingHours: 'Mo-Su 00:00-23:59',
+    priceRange: '$$',
+    description,
+  }
+
+  return (
+    <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{ __html: JSON.stringify(localBusiness) }}
+    />
+  )
+}
+
+export default function CarroattrezziFerraraPage() {
+  return (
+    <>
+      <JsonLd />
+      <BresciaLandingClient
+        phone={phone}
+        tel={tel}
+        city="Ferrara"
+        pagePath="/carroattrezzi-ferrara"
+      />
+    </>
+  )
+}
